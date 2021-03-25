9d85944eb053a74bd6a2646d58b10678d409562b
David Nugent <david.nugent2425@gmail.com>
2021-03-25 13:06:12 +0000

    run-simulator script now allows for optional choice of map

--- docker-setup/run-simulator.sh
+++ docker-setup/run-simulator.sh
@@ -1,4 +1,9 @@
 cd /catkin_ws
 catkin_make
 source devel/setup.bash
-roslaunch f110_simulator simulator.launch
+if [ $1 ]
+then
+	roslaunch f110_simulator simulator.launch map:=/catkin_ws/src/f110_ros/f110_simulator/maps/$1.yaml
+else
+	roslaunch f110_simulator simulator.launch
+fi
